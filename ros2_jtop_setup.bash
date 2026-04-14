0a887fa7b85b4ad75006bb913aee0d60cbf3f8aa
gtirth <109450810+gtirth@users.noreply.github.com>
2026-04-14 15:54:30 -0400

    Create ros2_jtop_setup.bash for Jetson setup
    Set up ROS 2 environment and service for Jetson.

---
+++ ros2_jtop_setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#please clonse this in colcon_ws on jetson
+source /opt/ros/humble/setup.bash
+cd ~/colcon_ws
+colcon build
+source install/setup.bash
+sudo cp ~/colcon_ws/src/ros2_jetson_stats/ros2_jtop.service /etc/systems/system/ros2_jtop.service
+sudo systemctl enable ros2_jtop.service
+sudo systemctl daemon_reload
+sudo systemctl start ros2_jtop.service
